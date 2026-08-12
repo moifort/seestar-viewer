9eee6710fdc23c29a30062e14003ed2943030a59
Thibaut <thibaut.mottet@gmail.com>
2026-08-12 15:55:40 +0200

    feat(kit): squelette du package SeestarKit et jeux d'essai réels

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 6.0
+import PackageDescription
+
+let package = Package(
+    name: "SeestarKit",
+    platforms: [.tvOS(.v17), .iOS(.v17), .macOS(.v14)],
+    products: [
+        .library(name: "SeestarKit", targets: ["SeestarKit"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/weichsel/ZIPFoundation.git", from: "0.9.19")
+    ],
+    targets: [
+        .target(
+            name: "SeestarKit",
+            dependencies: [.product(name: "ZIPFoundation", package: "ZIPFoundation")]
+        ),
+        .testTarget(
+            name: "SeestarKitTests",
+            dependencies: ["SeestarKit"],
+            resources: [.copy("Fixtures")]
+        )
+    ]
+)
